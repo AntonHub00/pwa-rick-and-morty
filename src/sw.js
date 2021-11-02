1da7a4055728a7f188afa5fbeae1e08452c9fae3
Antonio Ochoa <antonhub00@gmail.com>
2021-11-02 14:37:00 -0600

    ADD: Static assets to cache (install event SW)

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,24 @@
-self.addEventListener("install", async () => {});
+const staticCacheName = "static-assets-v1";
+
+const staticAssets = [
+  "/",
+  "/index.html",
+  "/js/index.js",
+  "/js/CardComponent.js",
+  "/js/CharacterDisplayer.js",
+  "/js/CharacterFetcher.js",
+  "/css/index.css",
+  "/images/notFound.jpg",
+];
+
+self.addEventListener("install", (event) => {
+  const preCache = async () => {
+    const staticCache = await caches.open(staticCacheName);
+    await staticCache.addAll(staticAssets);
+  };
+
+  event.waitUntil(preCache());
+});
 
 self.addEventListener("activate", async () => {});
 
